0a6961d8e37834fd1714802975674f1dbcc2d0d2
TreeOfSelf <53889485+TreeOfSelf@users.noreply.github.com>
2019-12-05 16:08:08 -0700

    Fixing up the ol shader

--- client/shaders.js
+++ client/shaders.js
@@ -16,17 +16,21 @@ out vec2 v_texcoord;
 void main() {
 
 
-	//float pixelX = ((u_blockRotation[0]*(a_position[0]-u_camera[0]) + u_blockRotation[1]*(a_position[1]-u_camera[1]))*(57.0));
-	//float pixelY = ((u_blockRotation[2]*(a_position[0]-u_camera[0]) + u_blockRotation[3]*(a_position[1]-u_camera[1]))*(45.0))-((a_position[2]-u_camera[2])*45.0);
 
 	gl_Position = vec4( 
+	//Isometric X Position
 	(((u_blockRotation[0]*(a_position[0]) + u_blockRotation[1]*(a_position[1]))*(28.0))
+	//Camera Offset , resolution and screen size adjustments 
 	-u_camera[0])*u_resolution*u_zoom/u_screenSize[0],
+	//Isometric Y Position
 	(((u_blockRotation[2]*(a_position[0]) + u_blockRotation[3]*(a_position[1]))*(13.5))-((a_position[2]-u_camera[2])*45.0)
+	//Camera Offset , resolution and screen size adjustments 
 	-u_camera[1])*u_resolution*u_zoom/u_screenSize[1],
 	0.0,
 	0.5);
+	//Set Depth based on final Y position and block Z
 	gl_Position[2] = (gl_Position[1] + (a_position[2]*100.0))*0.005;
+	//Size based on zoom 
 	gl_PointSize = 57.0 * min(1.0,u_zoom);
 	v_texcoord = a_texcoord;
 }
@@ -46,8 +50,8 @@ void main() {
 	//outColor = vec4(v_texcoord[0],v_texcoord[1],0.2,1.0);
 	//outColor = texture(u_sampler, v_texcoord);
 	outColor = texture(u_sampler,vec2(gl_PointCoord[0],gl_PointCoord[1]*1.26));
-	outColor.a *= step (0.999, outColor.a);
-	outColor.a -= step(0.78,gl_PointCoord[1]);
+	//This fixes the fuzzy alpha and makes the texture not a square
+	outColor.a *= step (0.999, outColor.a) - step(0.78,gl_PointCoord[1]);	
 }
 `;
 
